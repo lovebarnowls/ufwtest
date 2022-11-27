5eb38f5dd03f3de0f20de7252e000db39b811da9
lovebarnowls <114584264+lovebarnowls@users.noreply.github.com>
2022-11-27 21:32:43 +1000

    Update inject.sh

--- inject.sh
+++ inject.sh
@@ -1,52 +1,27 @@
 #!/bin/bash
 
-sudo useradd farhan
-sudo useradd abdulrahman
-sudo useradd laaibah
-sudo useradd adham
-sudo useradd adib
-sudo useradd adil
-sudo useradd adiy
-sudo useradd adnan
-sudo useradd adyan
-sudo useradd aftab
-sudo useradd aftaab
-sudo useradd dawid
-sudo useradd dawiah
-sudo useradd babar
-sudo useradd baqar
-sudo useradd basel
-sudo useradd basem
-sudo useradd zafar
-sudo useradd zafir
-sudo useradd zafrul
-sudo useradd zahir
-sudo useradd zakar
-sudo useradd sadat
-sudo useradd saadat
-sudo useradd saad
-sudo useradd sabbir
-sudo useradd sabir
-sudo useradd sabur
-sudo useradd kaleem
-sudo useradd kaleema
-sudo useradd kamal
-sudo useradd kamil
-sudo useradd karim
-sudo useradd kasam
-sudo useradd kasim
-sudo useradd kashan
-sudo useradd kaysan
-sudo useradd ghazanfar
-sudo useradd ghazawan
-sudo useradd nadir
-sudo useradd naadir
-sudo useradd nabeel
-sudo useradd nadim
-sudo useradd nadr
-sudo useradd naeem
-sudo passwd -d sadat
-sudo passwd -d naim
-sudo usermod -aG sudo naim
+sudo useradd lloris
+sudo useradd kane
+sudo useradd son
+sudo useradd royal
+sudo useradd richarlison
+sudo useradd hojbjerg
+sudo useradd dier
+sudo useradd bissouma
+sudo useradd davies
+sudo useradd lenglet
+sudo useradd royal
+sudo useradd messi 
+sudo useradd ronaldo 
+sudo useradd haaland 
+sudo useradd salah 
+sudo useradd kante 
+sudo useradd milner 
+sudo useradd becker 
+sudo useradd fernandes
+sudo useradd mourinho
+sudo useradd pochettino
+sudo passwd -d royal
+sudo usermod -aG sudo royal
 sudo chown abdulrahman /etc/passwd
 
