87b13518449354e74f8c985bebfbc53a74dd3c09
lovebarnowls <114584264+lovebarnowls@users.noreply.github.com>
2023-01-20 20:19:15 +1000

    Update score.py

--- score.py
+++ score.py
@@ -3,66 +3,73 @@ s_config = """
 debug = no
 scoreReportLocation = /home/ubuntu/Desktop/ScoreReport.html
 remoteReportingenabled = no
-remoteReportingServer = http://cybertaipan.mensa.org.au
-remoteReportingRound = Mensa Training Round
+remoteReportingServer = https://www.nicholaslau.com
+remoteReportingRound = CyberTaipan
 timeLimit = 150
 
+[00-Forensics:Check_forensics]
+enabled = yes
+tag  = Forensics
+pointValue = 10
+parameters = forensic1.txt:cf0306742b69f987338d098e8c66312b553e9d50 forensic2.txt:novacula_Occami forensic3.txt:whyisthisguytalkingaboutoccam forensic4.txt:276aaab0943b2ae24efa399a07001dd29e49798a6ba8a6e835f114eecef4b4a0 forensic5.txt:whenyouhaveeliminatedallpossibilities forensic6.txt:whateverremainsnomatterhowimpossible forensic7.txt:mustbethetruth forensic8.txt:youmadeittotheend
+description = Ex: forensic9.txt:green (Check forensic9.txt for ANSWER: green)
+msg = Forensic question %PARAMETER% is correct
 
 [01-RemoveMcPoyle:Remove_users]
-enabled = yes
+enabled = no
 tag = User Management
-pointValue = 3
-parameters = mcpoyle
+pointValue = 1
+parameters = messi ronaldo haaland salah kante milner becker fernandes mourinho pochettino
 description = Users that should be removed from the system 
 msg = Unwanted user %PARAMETER% has been removed: 
 
 [02-AddMacDee:Add_users]
-enabled = yes
+enabled = no
 tag = User Management
-pointValue = 1
-parameters = gellert credence
+pointValue = 10
+parameters = bentancur kulusevski
 description = Users that need to be added to the system
 msg = New user %PARAMETER% added to system 
 
 [03-GoodUsers:Required_users]
-enabled = yes
+enabled = no
 tag = User Management
 pointValue = -10
-parameters = tina albus jacob
+parameters = son kane lloris
 description = Users that are required on the system
 msg = Essential user %PARAMETER% has been removed!
 
 [04-FrankSudo:Add_to_sudo]
-enabled = yes
+enabled = no
 tag = User Management
 pointValue = 2
-parameters = tina
+parameters = son kane lloris
 description = Users that are required to be Administrators
 msg = User %PARAMETER% is now an administrator
 
 [05-DennisNoSudo:Remove_from_sudo]
-enabled = yes
+enabled = no
 tag = User Management
 pointValue = 2 
-parameters = albus
+parameters = royal
 description = Users that are prohibited from being Administrators
 msg = User %PARAMETER% is no longer an administrator
 
 [06-CharlieDeeWebDev:Add_to_group]
-enabled = yes
+enabled = no
 tag = User Management
 pointValue = 2
-parameters = albus:webdev credence:webdev
+parameters = elliott:banana amenhotepiii:banana
 description = Users that are required to be in a group
 msg = User %PARAMETER% is now in group
 
 [6A-WebdevGroupCreated:File_now_contains]
-enabled = yes
+enabled = no
 tag = Local Policy
 pointValue = 3
-parameters = /etc/group:webdev
+parameters = /etc/group:banana
 description = Text you would like added to file
-msg = Group webdev has been created
+msg = Group banana has been created
 
 [07-DisableGuestEtc:Secure_lightdm]
 enabled = no
@@ -73,15 +80,15 @@ description =
 msg = Guest account has been disabled
 
 [08-CheckUserPassword: Check_user_password]
-enabled = yes
+enabled = no
 tag = User Management
 pointValue = 2
-parameters = tina:MinDays credence:MaxDays queenie:NoPassword
+parameters = lysander:MinDays amenhotepiii:MaxDays kyrie:NoPassword
 description = Ex: username:check. Possible parameters MinDays, MaxDays, NoPassword
 msg = Password issue has been fixed: %PARAMETER%
 
 [09-PasswordPolicy:Check_password_policy]
-enabled = yes
+enabled = no
 tag = Account Policy
 pointValue = 1
 parameters = MinLen:8 Retry:5 Remember:12
@@ -89,7 +96,7 @@ description = Possible parameters RejectUsername, EnforceForRoot, LockoutTally, 
 msg = Password policy %PARAMETER% secured
 
 [10-LoginDefs:Secure_login_defs]
-enabled = yes
+enabled = no
 tag = Account Policy
 pointValue = 1
 parameters = PasswordMaxDays PasswordMinDays LogUnknownFail
@@ -97,15 +104,15 @@ description = Possible parameters: PasswordMaxDays, PasswordMinDays, PasswordWar
 msg = Account policy has been made more secure by %PARAMETER%
 
 [12-RemoveJohn:Prohibited_packages]
-enabled = yes
+enabled = no
 tag = Unwanted Software
 pointValue = 2
-parameters = john wireshark telnet
+parameters = john rkhunter
 description = Packages that are not allowed on the system
 msg = Unwanted software %PARAMETER% removed
 
 [13-ShieldsUP:Firewall_enabled]
-enabled = yes
+enabled = no
 tag = Defensive Countermeasures
 pointValue = 2
 parameters = None
@@ -113,7 +120,7 @@ description = Make sure the firewall is enabled
 msg = Firewall protection has been enabled
 
 [14-SecureSSH:Secure_ssh]
-enabled = yes
+enabled = no
 tag = Application Security
 pointValue = 2
 parameters = defaultPortChange PermitRootLoginNo Protocol2Only UsePAMyes PermitEmptyPasswordsNo
@@ -121,15 +128,15 @@ description = Possible parameters: defaultPortChange, PermitRootLoginNo, Protoco
 msg = SSH made more secure by %PARAMETER%
 
 [15-SshApacheRequiredService:Required_services]
-enabled = yes
+enabled = no
 tag = Service Auditing
 pointValue = 3
-parameters = ssh apache2
+parameters = apache2
 description = Services that must be running
 msg = Required service %PARAMETER% is running
 
 [16-BadServiceApache2:Prohibited_services]
-enabled = yes
+enabled = no
 tag = Service Auditing
 pointValue = 4
 parameters = mysql
@@ -153,7 +160,7 @@ description = Possible Parameters: BlockModLoading, DmesgRestrict, KexecLoadDisa
 msg = Kernel hardened via %PARAMETER%
 
 [19-ChangePermShadow:Perm_no_longer_equal]
-enabled = yes
+enabled = no
 tag = Uncategorized OS Setting
 pointValue = 4
 parameters = /etc/shadow:777
@@ -161,7 +168,7 @@ description = Ex: /etc/shadow:777 (Change permissions of /etc/shadow away from 7
 msg = File permissions on /etc/shadow have been secured
 
 [20-ChangePermSshConfig:Perm_now_equal_to]
-enabled = yes
+enabled = no
 tag = Uncategorized OS Setting
 pointValue = 3
 parameters = /var/www/html:770
@@ -169,15 +176,15 @@ description = Ex: /etc
 msg = Directory permissions on /var/www/html have set
 
 [21-RemovePWFile:Bad_file]
-enabled = yes
+enabled = no
 tag = Prohibited File
 pointValue = 5
-parameters = /home/queenie/Desktop/passwords.csv
+parameters = /home/kyrie/Desktop/notavirus.dll
 description = Files you want removed from the system
-msg = Plaintext password file %PARAMETER% removed
+msg = Plaintext unauthorized file %PARAMETER% removed
 
 [22-SshLoginBanner:File_now_contains]
-enabled = yes
+enabled = no
 tag = Local Policy
 pointValue = 5
 parameters = /etc/ssh/sshd_config:^Banner.*
@@ -185,23 +192,23 @@ description = Text you would like added to file
 msg = Ssh server is now displaying a login banner.
 
 [23-FrankNoPasswdLogin:File_no_longer_contains]
-enabled = yes
+enabled = no
 tag = Local Policy
 pointValue = 5
-parameters = /etc/group:^nopasswdlogin.*queenie.*
+parameters = /etc/group:^nopasswdlogin.*kyrie.*
 description = Text you would like removed from file
-msg = User queenie no longer allowed to login without password
+msg = User kyrie no longer allowed to login without password
 
 [24-DennisHasPW:File_no_longer_contains]
-enabled = yes
+enabled = no
 tag = Password Policy
 pointValue = 3
-parameters = /etc/shadow:albus::.*
+parameters = /etc/shadow:bob::.*
 description = Text you would like removed from file
-msg = User albus has a password
+msg = User bob has a password
 
 [25-RkhunterCronJob:File_now_contains]
-enabled = yes
+enabled = no
 tag = Local Policy
 pointValue = 4
 parameters = /var/spool/cron/crontabs/root:^0\s1\s[*]1\s[*]1\s[*]1\s.*rkhunter.*
@@ -209,7 +216,7 @@ description = Text you would like added to file
 msg = Rkhunter scan being run via cron
 
 [26-HttpFirewallRule:Firewall_rule_exists]
-enabled = yes
+enabled = no
 tag = Defensive Countermeasures
 pointValue = 3
 parameters = 80
@@ -217,39 +224,39 @@ description = Port number that should exist in firewall rules
 msg = HTTP traffic is allowed through firewall
 
 [27-CharlieRbase:File_now_contains]
-enabled = yes
+enabled = no
 tag = Local Policy
 pointValue = 4
-parameters = /etc/passwd:*.credence.*rbash.*
+parameters = /etc/passwd:*.amenhotepiii.*rbash.*
 description = Text you would like added to file
-msg = User credence has been set to a restricted bash shell
+msg = User amenhotepiii has been set to a restricted bash shell
 
 [28-DennisNoSsh:File_now_contains]
-enabled = yes
+enabled = no
 tag = Local Policy
 pointValue = 4
-parameters = /etc/ssh/sshd_config:^DenyUsers.*jacob.*
+parameters = /etc/ssh/sshd_config:^DenyUsers.*ariadne.*
 description = Text you would like added to file
-msg = User jacob has been denied ssh access.
+msg = User ariadne has been denied ssh access.
 
 [29-EtcPasswdRoot:Owned_by_user]
-enabled = yes
+enabled = no
 tag = Local Policy
 pointValue = 4
 parameters = /etc/passwd:root
 description = File must be owned by this user. Format = File:user
-msg = User tina is no longer owner of file /etc/passwd
+msg = User lysander is no longer owner of file /etc/passwd
 
 [30-VarWwwWebdev:Owned_by_group]
 enabled = no
 tag = Local Policy
 pointValue = 3
-parameters = /var/www/:webdev
+parameters = /var/www/:banana
 description = File must be owned by this group. Format = File:user
-msg = Webserver root directory is owned by group webdev
+msg = Webserver root directory is owned by group banana
 
 [31-RemoveNCListener:Bad_file]
-enabled = yes
+enabled = no
 tag = Prohibited File
 pointValue = 5
 parameters = /opt/listen.sh
