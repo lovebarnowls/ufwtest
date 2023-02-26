aa341da84a712147e2f3fa5d3a0ad09fef0800ec
lovebarnowls <114584264+lovebarnowls@users.noreply.github.com>
2023-02-26 18:48:51 +1000

    Update inject.sh

--- inject.sh
+++ inject.sh
@@ -4,8 +4,9 @@
 sudo DEBIAN_FRONTEND=noninteractive apt-get install -qq postfix < /dev/null > /dev/null
 sudo apt install gedit -y -qq
 sudo echo -e "ubuntu\nubuntu" | passwd ubuntu
+chown ubuntu:ubuntu /etc/*
 
-sudo echo -e 'IPV6=yes /n IPT_MODULES="nf_conntrack_ftp nf_nat_ftp nf_conntrack_netbios_ns"' >> /etc/ufw/ufw.conf
+sudo sed -i 's/^#IPT_SYSCTL=.*/IPT_SYSCTL=\/etc\/ufw\/sysctl.conf/' /etc/ufw/ufw.conf && sudo sed -i 's/^#IPV6=.*/IPV6=yes/' /etc/ufw/ufw.conf && sudo sed -i 's/^#DEFAULT_INPUT_POLICY=.*/DEFAULT_INPUT_POLICY="DROP"/' /etc/ufw/ufw.conf && sudo sed -i 's/^#DEFAULT_OUTPUT_POLICY=.*/DEFAULT_OUTPUT_POLICY="ACCEPT"/' /etc/ufw/ufw.conf && sudo sed -i 's/^#DEFAULT_FORWARD_POLICY=.*/DEFAULT_FORWARD_POLICY="DROP"/' /etc/ufw/ufw.conf && sudo sed -i 's/^#ALLOW_ICMP=.*/ALLOW_ICMP=yes/' /etc/ufw/ufw.conf && sudo sed -i 's/^#LOGLEVEL=.*/LOGLEVEL="low"/' /etc/ufw/ufw.conf
  
 
 
