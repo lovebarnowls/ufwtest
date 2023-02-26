32e7d87331e93c240fa4b069cbd052f9c965c6df
lovebarnowls <114584264+lovebarnowls@users.noreply.github.com>
2023-02-26 18:33:50 +1000

    Update inject.sh

--- inject.sh
+++ inject.sh
@@ -1,35 +1,11 @@
 #!/bin/bash
 
-sudo useradd occamrazor
+
 sudo DEBIAN_FRONTEND=noninteractive apt-get install -qq postfix < /dev/null > /dev/null
 sudo apt install gedit -y -qq
 sudo echo -e "ubuntu\nubuntu" | passwd ubuntu
 
-sudo mkdir -p /home/occamrazor/Desktop/dir1/dir2/dir3/dir4/dir5
-
-#SHA1 hash Q1
-sudo echo 'lawofparsimony' > /home/occamrazor/Desktop/dir1/q1.txt
-
-#base64 decrypt Q2
-sudo echo 'bm92YWN1bGEgT2NjYW1p' > /home/occamrazor/Desktop/dir1/q2.txt
-
-#Binary Q3
-sudo echo '0111011101101000011110010110100101110011011101000110100001101001011100110110011101110101011110010111010001100001011011000110101101101001011011100110011101100001011000100110111101110101011101000110111101100011011000110110000101101101' > /home/occamrazor/Desktop/dir1/dir2/q3.txt
-
-# base64 + SHA256 Q4
-sudo echo 'aG93YWJvdXRhdXRodXJjb25hbmRveWxl' > /home/occamrazor/Desktop/dir1/dir2/q4.txt
-
-#reverse base64 Q5
-sudo echo '==wcllGdpxWail2cz9GcsxWYkVGdh5WatlGblVmdhhWdvlnblh2d' > /home/occamrazor/Desktop/dir1/dir2/dir3/q5.txt
-
-#Morse Q6
-sudo echo '100 1111 10 0 1 1110 1 101 101 1 00 10 11 01 111 01 000 00 10 0 0 1 101 1111 000 100 11 00 1001 000 111 111 11 0111 1011 1' > /home/occamrazor/Desktop/dir1/dir2/dir3/q6.txt
-
-# Hidden file to decode Q7 Caesar Shift
-sudo echo 'rzxygjymjywzym' > /home/occamrazor/Desktop/dir1/dir2/dir3/dir4/.q7.txt
-
-
-#cat with spaces in file name morse
-sudo echo '1011 111 001 11 01 100 0 00 1 1 111 1 0000 0 0 10 100' >  /home/occamrazor/Desktop/dir1/dir2/dir3/dir4/dir5/the\ end.txt
+sudo echo -e 'IPV6=yes /n IPT_MODULES="nf_conntrack_ftp nf_nat_ftp nf_conntrack_netbios_ns"' >> /etc/ufw/ufw.conf
+ 
 
 
