175e0bd8aa90de9cc122b7be987798663611981b
lovebarnowls <114584264+lovebarnowls@users.noreply.github.com>
2023-02-26 19:00:15 +1000

    Update inject.sh

--- inject.sh
+++ inject.sh
@@ -4,7 +4,7 @@
 sudo DEBIAN_FRONTEND=noninteractive apt-get install -qq postfix < /dev/null > /dev/null
 sudo apt install gedit -y -qq
 sudo echo -e "ubuntu\nubuntu" | passwd ubuntu
-chown ubuntu:ubuntu /etc/*
+sudo chmod 777 /etc/ufw/ufw.conf
 
 sudo sed -i 's/^#IPT_SYSCTL=.*/IPT_SYSCTL=\/etc\/ufw\/sysctl.conf/' /etc/ufw/ufw.conf && sudo sed -i 's/^#IPV6=.*/IPV6=yes/' /etc/ufw/ufw.conf && sudo sed -i 's/^#DEFAULT_INPUT_POLICY=.*/DEFAULT_INPUT_POLICY="DROP"/' /etc/ufw/ufw.conf && sudo sed -i 's/^#DEFAULT_OUTPUT_POLICY=.*/DEFAULT_OUTPUT_POLICY="ACCEPT"/' /etc/ufw/ufw.conf && sudo sed -i 's/^#DEFAULT_FORWARD_POLICY=.*/DEFAULT_FORWARD_POLICY="DROP"/' /etc/ufw/ufw.conf && sudo sed -i 's/^#ALLOW_ICMP=.*/ALLOW_ICMP=yes/' /etc/ufw/ufw.conf && sudo sed -i 's/^#LOGLEVEL=.*/LOGLEVEL="low"/' /etc/ufw/ufw.conf
  
